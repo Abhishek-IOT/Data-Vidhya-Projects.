207b5b0f4199867dd290d3e67b6ca8419fd63a08
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2025-09-19 16:55:22 +0530

    Created Template for Ec2 Deployment with custom task of installing kafka

--- Ride Stream Real-Time Data Lakehouse/Kafka_Setup.sh
+++ Ride Stream Real-Time Data Lakehouse/Kafka_Setup.sh
@@ -22,7 +22,7 @@ wget https://github.com/aws/aws-msk-iam-auth/releases/download/v2.3.0/aws-msk-ia
 
 security.protocol=SASL_SSL
 sasl.mechanism=AWS_MSK_IAM
-sasl.jaas.config=software.amazon.msk.auth.iam.IAMLoginModule required;
+sasl.jaas.config=software.amazon.msk.auth.iam.IAMLoginModule required
 sasl.client.callback.handler.class=software.amazon.msk.auth.iam.IAMClientCallbackHandler
 
 #Export CLASSPATH

a276d1ee6164bfc4892861f6045ffad483850dbb
Abhishek Srivastav <53862677+Abhishek-IOT@users.noreply.github.com>
2026-01-14 21:55:08 +0530

    Refactor delete procedure with validations and logging
    Refactor execute_delete_statement procedure to include mandatory validations for target table and WHERE clause. Update the audit logging to reflect records updated instead of deleted.

--- FoodOrder Analytics Hub/Stored_Procedure_delete.sql
+++ FoodOrder Analytics Hub/Stored_Procedure_delete.sql
@@ -1,32 +1,54 @@
 CALL execute_delete_statement(
-    'delete from BUSINESS.fact_order_patterns where restaurant_key=253','fact_order_patterns'
+  'BUSINESS',
+  'fact_order_patterns',
+  'DATE_KEY < 20230101'
 );
 
-DROP PROCEDURE execute_delete_statement;
 
+DROP PROCEDURE execute_delete_statement;
 
 DELIMITER $$
 
 CREATE PROCEDURE execute_delete_statement (
-    IN p_sql TEXT,IN Table_Names TEXT
+    IN p_target_schema VARCHAR(64),
+    IN p_target_table  VARCHAR(64),
+    IN p_where_clause  TEXT
 )
 BEGIN
-	declare v_load_start_time DATETIME;
-	declare p_rows_deleted INT;
-    -- Move local variable into session variable
-    SET @sql_text = p_sql;
+    DECLARE v_load_start_time DATETIME;
+    DECLARE v_rows_deleted INT;
+    DECLARE v_sql TEXT;
 
-    -- Prepare from session variable
-    PREPARE stmt FROM @sql_text;
-	set v_load_start_time=NOW();
+    -- Mandatory validations
+    IF p_target_table IS NULL OR p_target_table = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Target table is mandatory';
+    END IF;
+
+    IF p_where_clause IS NULL OR TRIM(p_where_clause) = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'WHERE clause is mandatory for DELETE';
+    END IF;
+
+    SET v_load_start_time = NOW();
+
+    -- Build DELETE SQL
+    SET v_sql = CONCAT(
+        'DELETE FROM ',
+        p_target_schema, '.', p_target_table,
+        ' WHERE ',
+        p_where_clause
+    );
+
     -- Execute dynamic SQL
+    SET @sql_text = v_sql;
+    PREPARE stmt FROM @sql_text;
     EXECUTE stmt;
-
-    SET p_rows_deleted = ROW_COUNT();
-    -- Cleanup
+    SET v_rows_deleted = ROW_COUNT();
     DEALLOCATE PREPARE stmt;
-    
-       INSERT INTO Stage.etl_load_audit (
+
+    -- Audit logging
+    INSERT INTO Stage.etl_load_audit (
         job_name,
         source_schema,
         source_table,
@@ -34,20 +56,20 @@ BEGIN
         target_table,
         load_start_time,
         load_end_time,
-        records_deleted,
+        records_updated,
         load_status,
         executed_by,
         execution_date
     )
     VALUES (
-        'UPDATION_LOAD',
+        'DELETION_LOAD',
         'Stage',
         'NA',
-        'BUSINESS',
-        Table_Names,
+        p_target_schema,
+        p_target_table,
         v_load_start_time,
         NOW(),
-        p_rows_deleted,
+        v_rows_deleted,
         'SUCCESS',
         'ETL_LOAD',
         CURDATE()

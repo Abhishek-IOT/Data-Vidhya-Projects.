9e79d6d78e94178a88b28c2df525fe8374cd1715
Abhishek Srivastav <53862677+Abhishek-IOT@users.noreply.github.com>
2026-01-19 11:35:21 +0530

    Update Job_Config_Logs.sql

--- FoodOrder Analytics Hub/SQL_Database_Fabric/Job_Config_Logs.sql
+++ FoodOrder Analytics Hub/SQL_Database_Fabric/Job_Config_Logs.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Stage.etl_load_audit (
-    audit_id BIGINT varchar(MAX),
+CREATE  TABLE Stage.etl_load_audit (
+    audit_id VARCHAR(MAX),
     job_name VARCHAR(100),
     source_schema VARCHAR(50),
     source_table VARCHAR(100),

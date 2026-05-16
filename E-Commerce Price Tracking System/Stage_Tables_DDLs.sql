e594a2c8004bcc28dc124d185c327781998867d0
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2026-05-16 17:52:19 +0530

    Stage Layer DDL

---
+++ E-Commerce Price Tracking System/Stage_Tables_DDLs.sql
@@ -0,0 +1,54 @@
+create database DEVELOPMENT;
+create schema STAGE;
+
+
+CREATE OR REPLACE TABLE STAGE.STG_CUSTOMERS (
+    CUSTOMER_ID            STRING,
+    CUSTOMER_NAME          STRING,
+    EMAIL                  STRING,
+    PHONE                  STRING,
+    CITY                   STRING,
+    STATE                  STRING,
+    COUNTRY                STRING,
+    REGISTERED_DATE        TIMESTAMP,
+    CUSTOMER_STATUS        STRING,
+    LOAD_DATE              TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    RECORD_SOURCE          STRING
+);
+
+
+CREATE OR REPLACE TABLE STAGE.STG_PRODUCTS (
+    PRODUCT_ID             STRING,
+    PRODUCT_NAME           STRING,
+    CATEGORY               STRING,
+    BRAND                  STRING,
+    BASE_PRICE             NUMBER(10,2),
+    PRODUCT_STATUS         STRING,
+    LOAD_DATE              TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    RECORD_SOURCE          STRING
+);
+
+
+
+CREATE OR REPLACE TABLE STAGE.STG_PRICE_UPDATES (
+    PRODUCT_ID             STRING,
+    PRICE                  NUMBER(10,2),
+    DISCOUNT_PERCENT       NUMBER(5,2),
+    FINAL_PRICE            NUMBER(10,2),
+    PRICE_UPDATE_TIME      TIMESTAMP,
+    LOAD_DATE              TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    RECORD_SOURCE          STRING
+);
+
+
+
+CREATE OR REPLACE TABLE STAGE.STG_ORDERS (
+    ORDER_ID               STRING,
+    CUSTOMER_ID            STRING,
+    ORDER_DATE             TIMESTAMP,
+    ORDER_STATUS           STRING,
+    TOTAL_AMOUNT           NUMBER(12,2),
+    LOAD_DATE              TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    RECORD_SOURCE          STRING
+);
+

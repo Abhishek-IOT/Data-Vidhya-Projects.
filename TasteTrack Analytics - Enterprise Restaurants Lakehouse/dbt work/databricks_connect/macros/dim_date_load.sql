52cfd9b7f7618511938494b593be2278aeda04d8
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2026-08-02 21:46:40 +0530

    Dim Date Table Load Created

---
+++ TasteTrack Analytics - Enterprise Restaurants Lakehouse/dbt work/databricks_connect/macros/dim_date_load.sql
@@ -0,0 +1,40 @@
+{% macro dim_employee_load() %}
+
+{% set sql %}
+
+
+
+INSERT INTO gold_layer.DIM_DATE
+SELECT
+    CAST(date_format(FULL_DATE, 'yyyyMMdd') AS BIGINT) AS DATE_KEY,
+    FULL_DATE,
+    date_format(FULL_DATE, 'dd') AS DAY_NUMBER,
+    month(FULL_DATE) AS MONTH_NUMBER,
+    date_format(FULL_DATE, 'MMMM') AS MONTH_NAME,
+    CONCAT('Q', quarter(FULL_DATE)) AS QUARTER_NUMBER,
+    year(FULL_DATE) AS YEAR_NUMBER,
+    current_timestamp() AS LOAD_DATE
+FROM (
+    SELECT EXPLODE(
+        SEQUENCE(
+            TO_DATE('2020-01-01'),
+            CURRENT_DATE(),
+            INTERVAL 1 DAY
+        )
+    ) AS FULL_DATE
+);
+
+
+
+
+
+
+
+
+{% endset %}
+
+{% do run_query(sql) %}
+
+{{ log("Dim Date LOAD COMPLETED completed successfully.", info=True) }}
+
+{% endmacro %}
